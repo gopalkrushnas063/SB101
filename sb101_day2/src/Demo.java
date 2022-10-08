80ad69b17d4d0b14ce40f2e6159ca2b3d0c189d7
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-10-08 21:01:44 +0530

    array print

---
+++ sb101_day2/src/Demo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,30,40,50};
+        System.out.println(Arrays.toString(arr));
+    }
+}
